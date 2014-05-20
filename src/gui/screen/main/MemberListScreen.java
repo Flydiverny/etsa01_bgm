40bfa8ca8d53863e474fd8b0d672376295d4e1b2
JohnCaboose <johncaboose@live.com>
2014-05-21 01:07:13 +0200

    noo

--- src/gui/screen/main/MemberListScreen.java
+++ src/gui/screen/main/MemberListScreen.java
@@ -1,10 +1,20 @@
 package gui.screen.main;
 
-import java.awt.BorderLayout;
+import interfaces.IMember;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
+import org.w3c.dom.views.AbstractView;
+
+import gui.MainGUI;
 import gui.base.Screen;
 
 public class MemberListScreen extends Screen {
@@ -14,19 +24,71 @@ public class MemberListScreen extends Screen {
 	@Override
 	public void create() {
 		setLayout(new BorderLayout());	
-		String[] columnNames = {
-									"Social Security Number",
-					                "Name",
-					                "Registered Bicycles",
-					                "Checked In Bicycles",
-					                "Enabled"
-            					};
-		Object[][] data = new Object[memberManager.amountOfMembers()][5];
-		JTable table = new JTable(data, columnNames);
+		
+		
+		JButton selectButton = new JButton("Go to details page for selected member");
+		
+		
+		final JTable table = new JTable(new TableModelBGM());
 		JScrollPane scrollPane = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
+		
+		selectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				MainGUI.getInstance().setScreen(new MemberScreen(memberManager.getMember((String) table.getValueAt(table.getSelectedRow(), 0))));
+				//System.out.println((String) table.getValueAt(table.getSelectedRow(), 0));
+			}
+		});
+		
 		add(scrollPane, BorderLayout.CENTER);
+		add(selectButton,BorderLayout.SOUTH);
 		
 	}
+	private class TableModelBGM extends AbstractTableModel{
 
+		private static final long serialVersionUID = 2083509527767319524L;
+		String[] columnNames = {
+				"Social Security Number",
+	            "Name",
+	            "Registered Bicycles",
+	            "Enabled"
+			};
+		private Object[][] data = new Object[memberManager.amountOfMembers()][columnNames.length];
+		
+		private TableModelBGM(){
+			//get all the datazzz
+			List<IMember> memberlist = memberManager.listMembers();
+			int i = 0;
+			for(IMember m : memberlist){
+				data[i][0] = m.getSSN();
+				data[i][1] = m.getName();
+				data[i][2] = m.amountOfBicycles();
+				data[i][3] = !m.isDisabled();
+				i++;
+			}
+		}
+
+		public int getColumnCount() {
+	        return columnNames.length;
+	    }
+	    public int getRowCount() {
+	        return data.length;
+	    }
+
+	    public String getColumnName(int col) {
+	        return columnNames[col];
+	    }
+
+	    public Object getValueAt(int row, int col) {
+	        return data[row][col];
+	    }
+
+	    public Class getColumnClass(int c) {
+	        return getValueAt(0, c).getClass();
+	    }
+	}
 }
+
+
+
