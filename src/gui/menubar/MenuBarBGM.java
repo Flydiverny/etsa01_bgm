f47bd044b4b003aa0381ae98878f0e092ee727f2
JohnCaboose <johncaboose@live.com>
2014-05-20 20:15:22 +0200

    beginnings of menubar

---
+++ src/gui/menubar/MenuBarBGM.java
@@ -0,0 +1,21 @@
+package gui.menubar;
+
+import java.awt.MenuBar;
+
+public class MenuBarBGM extends MenuBar {
+	private static final long serialVersionUID = -4255644508622465982L;
+	public final static int MENUBAR_TYPE_LOCKED_DOWN = 0, MENUBAR_TYPE_EVERYTHING_ALLOWED = 1;
+	public MenuBarBGM(int menubartype){
+		if(menubartype==MENUBAR_TYPE_LOCKED_DOWN){
+			//TODO bare minimum sorta
+		}
+		if(menubartype == MENUBAR_TYPE_EVERYTHING_ALLOWED){
+			//TODO add everything
+			
+		}
+		
+		
+	}
+
+
+}
