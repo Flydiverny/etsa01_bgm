5404a9845a94f1f306018396935e6c8bc3d3720b
Erik Hedin <bts13ehe@jupiter-5.student.lth.se>
2014-05-20 17:16:53 +0200

    Made some tests :)))

---
+++ src/test/UnregisterMemberTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import system.MemberManager;
+
+public class UnregisterMemberTest {
+
+	@Test
+	public void noreg() {
+		MemberManager member = new MemberManager();
+		member.createMember("Jacob Nilsson", "Jupitergatan 2", "070315232",
+				"199309245151");
+		
+		member.getMember("199309245151");
+		
+		assertTrue("user removed", member.removeMember("199309245151"));
+		assertNull("user not registered", member.getMember("199309245151"));
+	}
+
+}
