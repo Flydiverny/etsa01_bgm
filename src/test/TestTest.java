b58634f7e50802abbc2f937e6c04e65245f25c2c
Simon Holk <bts13sho@jupiter-3.student.lth.se>
2014-05-20 15:47:28 +0200

    added test 4

--- src/test/TestTest.java
+++ src/test/TestTest.java
@@ -1,39 +1,32 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import system.MemberManager;
 //import interfaces.Bicycle;
 //import interfaces.Member;
 //import interfaces.MemberManager;
-import interfaces.IMember;
-import interfaces.hardware.BarcodePrinter;
-import interfaces.hardware.ElectronicLock;
-import interfaces.hardware.PinCodeTerminal;
-import system.BicycleGarageManager;
-import system.Member;
-import system.MemberManager;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import org.junit.Before;
-import org.junit.Test;
 
 public class TestTest {
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		String operatorPassword = null;
 		int userPIN = 0;
 		int operatorPIN = 0;
 		boolean userStatus = false;
-		
+		MemberManager manager = new MemberManager();
 		//boolean userRegistered = false;
 	}
+	 @After
+	 public void tearDown() {
+		 
+	 }
 	
 	/* Test 1
 	 * User register as a member of the bicycle garage and registers a bike
@@ -41,11 +34,35 @@ public class TestTest {
 	@Test
 	public void userReg() {
 		//insert code to press "create new member" button
+		System.out.println("TEST 1 ------------");
 		MemberManager manager = new MemberManager();
 		manager.createMember("Jacob Nilsson", "Jupitergatan 2", "070315232",
 				"199309245151");
-		assertEquals("name is not the same", "Jacob Nilsson", manager.getMember("199309245151").getName() );
+		assertEquals("Name is not the same", "Jacob Nilsson", manager.getMember("199309245151").getName() );
+		assertEquals("Address is not the same", "Jupitergatan 2", manager.getMember("199309245151").getAddress() );
+		assertEquals("Phone is not the same", "070315232", manager.getMember("199309245151").getPhone() );
+		
+		assertEquals("should be 1 bicycle registered", new Integer(0), new Integer(manager.getMember("199309245151").amountOfBicycles()) );
+		manager.getMember("199309245151").registerBicycle("Yellow ladies bicycle");
+		assertEquals("should be 1 bicycle registered", new Integer(1), new Integer(manager.getMember("199309245151").amountOfBicycles()) );
+		String barcode = manager.getMember("199309245151").getBicycles().get(0).getBarcode();
+		assertNotNull( barcode);
+		System.out.println("barcode: " + barcode);
+		//system clock?
+		System.out.println("TEST 1 ------------");
 		
 	}
+	/* Test 4
+	 * Member retrieves bicycle from the garage.
+	 */
+	@Test
+	public void userCheckOut() {
+		MemberManager manager = new MemberManager();
+		manager.createMember("Jacob Nilsson", "Jupitergatan 2", "070315232", "199309245151");
+		manager.getMember("199309245151").registerBicycle("Yellow ladies bicycle");
+		boolean apa = true;
+		manager.getMember("199309245151").getBicycles().get(0).setCheckedIn(apa);
+	}
+	
 
 }
