78fc45eca2491db3dab57abb4b84c9201d17ca75
Markus Maga <markus@nadilus.com>
2014-05-20 16:11:00 +0200

    working on parameter sceren

--- src/gui/screen/installation/ParameterScreen.java
+++ src/gui/screen/installation/ParameterScreen.java
@@ -1,12 +1,18 @@
 package gui.screen.installation;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import gui.InstallationGUI;
 import gui.base.Screen;
@@ -15,14 +21,15 @@ public class ParameterScreen extends Screen {
 
 	@Override
 	public void create() {
-		JLabel welcomeTxt = new JLabel("Welcome to Bicycle Garage Management");
-		JLabel descTxt = new JLabel("This installation wizard will guide you through the setup of the magic bicycle management system.");
+		JLabel welcomeTxt = new JLabel("System Parameters");
+		JLabel descTxt = new JLabel("To install the system we need some starting information.");
+		
 		welcomeTxt.setFont(welcomeTxt.getFont().deriveFont(20f));
+		
 		JPanel southPanel = new JPanel();
 		
 		JButton cancelBtn = new JButton();
 		cancelBtn.setText("Cancel");
-		
 		cancelBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -47,9 +54,39 @@ public class ParameterScreen extends Screen {
 		this.setLayout(new BorderLayout());
 		
 		add(welcomeTxt, BorderLayout.NORTH);
-		add(descTxt, BorderLayout.CENTER);
+		add(centerPanel(), BorderLayout.CENTER);
 		add(southPanel, BorderLayout.SOUTH);
 	}
 
+	private JPanel centerPanel() {
+		JPanel pane = new JPanel();
+		
+		GridBagConstraints c = new GridBagConstraints();
+		pane.setLayout(new GridBagLayout());
+		
+		JLabel bicycles = new JLabel("Bicycle");	
+		c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 0;
+	    c.gridy = 0;
+		pane.add(bicycles,c);
+				
+		JTextField bicyclesInput = new JTextField();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+	    c.gridy = 0;
+		pane.add(bicyclesInput,c);
+		
+		JLabel oppwd = new JLabel("Operator Password");
+		c.gridx = 0;
+	    c.gridy = 1;
+		pane.add(oppwd,c);
+		
+		JLabel oppwdrep = new JLabel("Repeat Password");
+		c.gridx = 0;
+	    c.gridy = 2;
+		pane.add(oppwdrep,c);
+		
+		return pane;
+	}
 	
 }
