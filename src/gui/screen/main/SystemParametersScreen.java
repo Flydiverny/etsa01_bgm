030bd6bc324fb306c5eb2591f263738b662f7862
JohnCaboose <johncaboose@live.com>
2014-05-20 15:30:24 +0200

    created system parameters screen

---
+++ src/gui/screen/main/SystemParametersScreen.java
@@ -0,0 +1,18 @@
+package gui.screen.main;
+
+import gui.base.Screen;
+
+public class SystemParametersScreen extends Screen {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8791422727411899812L;
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
