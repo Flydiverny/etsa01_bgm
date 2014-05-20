825f47437410ff97514a5c46fc6a3731c685d153
JohnCaboose <johncaboose@live.com>
2014-05-20 17:38:18 +0200

    created main screen hittepå (shell) och typ klar med system parameters screen

---
+++ src/gui/screen/main/MainScreen.java
@@ -0,0 +1,18 @@
+package gui.screen.main;
+
+import gui.base.Screen;
+
+public class MainScreen extends Screen {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1779944875318021304L;
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
