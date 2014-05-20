94b63fe47b7bd53eae4884298a6b337dbe1a5fc4
JohnCaboose <johncaboose@live.com>
2014-05-20 21:34:50 +0200

    loginscreen has begun. be very afraid

--- src/gui/screen/main/LoginScreen.java
+++ src/gui/screen/main/LoginScreen.java
@@ -1,5 +1,16 @@
 package gui.screen.main;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import gui.MainGUI;
 import gui.base.Screen;
 
 public class LoginScreen extends Screen {
@@ -8,5 +19,31 @@ public class LoginScreen extends Screen {
 	public void create() {
 		// TODO Auto-generated method stub
 		
+		JPanel southPanel = new JPanel();
+		
+		final JTextField jtfpassword = new JTextField();
+		JLabel jlpassword = new JLabel("Password");
+		JButton nextBtn = new JButton();
+		nextBtn.setText("Login");
+		
+		JPanel inputpanel = new JPanel();
+		inputpanel.setLayout(new BorderLayout());
+		
+		nextBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(bgm.loginOperator(jtfpassword.getText())){
+					MainGUI.getInstance().setScreen(new MainScreen());
+				}else{
+					JOptionPane.showMessageDialog(LoginScreen.this, "Invalid password");
+					return;
+				}
+			}
+		});
+		
+		southPanel.setLayout(new BorderLayout());
+		southPanel.add(nextBtn, BorderLayout.EAST);
+		this.add(southPanel, BorderLayout.SOUTH);
 	}
 }
